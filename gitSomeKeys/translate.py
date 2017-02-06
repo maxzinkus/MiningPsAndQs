0283a88ff260f82bd6de5f3b644509b4c72e433d
Nick Gonella <ngonella@calpoly.edu>
2017-02-06 00:36:42 +0000

    reformated modulus

---
+++ gitSomeKeys/translate.py
@@ -0,0 +1,7 @@
+txt = open('output.txt', 'r').read()
+out = open('mods.txt', 'w')
+
+for line in txt.split('\n'):
+   print(line)
+   out.write(str(int(''.join(line.split(':')), 16)))
+   out.write('\n')
